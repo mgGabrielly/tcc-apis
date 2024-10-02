cfb10c850428192ab9a86b4336520e247c37b653
Maria Gabrielly <104575546+mgGabrielly@users.noreply.github.com>
2024-10-01 23:17:46 -0300

    Update Program.cs

--- CoreNet/Program.cs
+++ CoreNet/Program.cs
@@ -2,25 +2,26 @@ using CoreNet.Data;
 using CoreNet.Repositories;
 using CoreNet.Services;
 using Microsoft.EntityFrameworkCore;
-using DotNetEnv; // Adicione esta linha
+using DotNetEnv; 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Carregar variáveis do arquivo .env
-Env.Load(); // Adicione esta linha
+Env.Load(); 
 
 builder.Services.AddControllers();
 
-// Usar a string de conexão do .env, com um fallback
-var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL") ?? 
-                       builder.Configuration.GetConnectionString("DefaultConnection");
+var host = Environment.GetEnvironmentVariable("POSTGRES_HOST"); 
+var database = Environment.GetEnvironmentVariable("POSTGRES_DB"); 
+var username = Environment.GetEnvironmentVariable("POSTGRES_USER"); 
+var password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD"); 
+
+var connectionString = $"Host={host};Database={database};Username={username};Password={password}";
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 
 builder.Services.AddScoped<IBookRepository, BookRepository>();
 builder.Services.AddScoped<BookService>();
-
 builder.Services.AddScoped<IGenreRepository, GenreRepository>();
 builder.Services.AddScoped<GenreService>();
 
